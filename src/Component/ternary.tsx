1ed701357d7bf07efa6f4184ac7ebc6d44e02206
velduraijavafullstack <veldurai232@gmail.com>
2023-10-10 11:09:59 +0530

    conditional statement

--- src/Component/ternary.tsx
+++ src/Component/ternary.tsx
@@ -1,17 +1,20 @@
 function Ternary() {
-  const arrays = [];
+  const arrays = obj.val;
 
   return (
     <>
       <h3>Ternary</h3>
-      {arrays.length === 0 ? <p>Array is empty</p> : null}
+      {/* {arrays.length === 0 ? <p>Array is empty</p> : null} */}
       {arrays.length === 0 && <p>Array is not empty</p>}
       <ul className="list-group">
-        {arrays.map((val) => (
-          <p>{val}</p>
+        {arrays.map((array) => (
+          <li className="list-group-item">{array}</li>
         ))}
       </ul>
     </>
   );
 }
 export default Ternary;
+const obj = {
+  val: ["val1", "val2", "val3"],
+};
