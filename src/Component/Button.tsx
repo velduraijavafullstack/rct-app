3480b01958ab9f23f47f0af7d4edbcb844f2b012
velduraijavafullstack <veldurai232@gmail.com>
2023-10-20 22:37:56 +0530

    react

--- src/Component/Button.tsx
+++ src/Component/Button.tsx
@@ -1,5 +1,20 @@
-const Button = () => {
-  return <button className="btn btn-primary">Button</button>;
+interface Props {
+  children: string;
+  color: string;
+  onClick: () => void;
+}
+
+const Button = ({ children, color, onClick }: Props) => {
+  return (
+    <div>
+      <button
+        className={"btn btn" + color}
+        onClick={(onClick = () => console.log("clicked"))}
+      >
+        {children}
+      </button>
+    </div>
+  );
 };
 
 export default Button;
